9e8bca573b001fea9f6de5b4ffcd2d9453c3d214
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-07 19:37:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,7 @@ def badge_maker(Arel)
 end 
 
 def batch_badge_creator(some_array)
-  some_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  some_array = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 end
+
+def assign_rooms
