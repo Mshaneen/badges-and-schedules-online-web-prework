5da5731266aed1678fb78fba2fab54dc83f62aa3
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-07 19:35:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,6 @@ def badge_maker(Arel)
   puts "Hello, my name is Arel."
 end 
 
-def batch_badge_creator
+def batch_badge_creator(some_array)
+  some_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+end
